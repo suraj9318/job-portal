3403ef45a2f45fde144cec6a7f1ac13c634624a0
suraj <surajmishra9318@gmail.com>
2023-01-24 10:42:53 +0530

    search Container return and allJobState

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -60,6 +60,13 @@ const allJobsSlice = createSlice({
     hideLoading : (state)=>{
       state.isLoading = true
     },
+    handleChange : (state,{payload:{name,value}})=>{
+      // state.page = 1;
+      state[name] = value;
+    },
+    clearFilters : (state)=>{
+      return {...state, ...initialFiltersState}
+    }
   },
   extraReducers : {
     [getAllJobs.pending] : (state) =>{
@@ -92,5 +99,5 @@ const allJobsSlice = createSlice({
   }
 })
 
-export const {showLoading, hideLoading} = allJobsSlice.actions;
+export const {showLoading, hideLoading, handleChange, clearFilters} = allJobsSlice.actions;
 export default allJobsSlice.reducer;
